ede6fe3f7bc9596b1d1128bba34202664f07135d
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-03-26 19:17:34 -0400

    feat: add decrement buttton

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const reducer = (state, action) => {
   switch(action.type) { 
     case 'INCREMENT':
       return { count: state.count + 1 } 
+    case 'DECREMENT':
+      return { count: state.count - 1 } 
       default:
         throw new Error();
   }
@@ -12,12 +14,13 @@ const reducer = (state, action) => {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, { count: 0 })
-  // INTIAL STEP UP
+  // INTIAL STEP UP FOR USE STATE
   // const [count, setCount] = useState(0);
   return (
     <>
       Count: { state.count }
       <button onClick={()=> dispatch({type: "INCREMENT"}) }>+</button>
+      <button onClick={()=> dispatch({type: "DECREMENT"}) }>-</button>
       {/* Count: {count}
       <button onClick={()=> setCount(count+1) }>+</button> */}
     </>
